ab26d833b40e2e9443a3329a543f2485bf03d579
Aman Painuly <amanpainuly69@gmail.com>
2026-04-13 11:32:50 +0530

    fixed a typo

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -111,6 +111,8 @@ interface RedditSearchChild {
     num_comments: number;
     permalink: string;
   };
+}
+
 import * as cheerio from 'cheerio';
 
 const COMMON_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36';
